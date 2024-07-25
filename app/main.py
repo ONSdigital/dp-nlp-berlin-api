5d6e821220491f3e4407c13c140133a275b7e2a5
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2024-07-25 15:01:07 +0100

    Add startup config log

--- app/main.py
+++ app/main.py
@@ -3,12 +3,21 @@ from flask import Flask
 from app.settings import settings
 from app.views.berlin import berlin_blueprint
 from app.views.health import health_blueprint
+from app.logger import logger
 
 
 def create_app():
     application = Flask(__name__)
     application.register_blueprint(berlin_blueprint)
     application.register_blueprint(health_blueprint)
+    logger.info("startup configuration", data={
+        "HOST": settings.HOST,
+        "PORT": settings.PORT,
+        "NAMESPACE": settings.NAMESPACE,
+        "DATA_LOCATION": settings.DATA_LOCATION,
+        "BUILD_TIME": settings.BUILD_TIME,
+        "GIT_COMMIT": settings.GIT_COMMIT,
+    })
     return application
 
 
