fca44f83bb3d308e9bee79c7386084fe81a6b634
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-01-23 15:52:37 +0000

    Lock poetry version

--- ci/scripts/deps.sh
+++ ci/scripts/deps.sh
@@ -5,7 +5,7 @@ if command -v poetry &> /dev/null; then
     poetry install
 else
     echo "Poetry not found. Installing Poetry and then running poetry install."
-    pip install poetry
+    pip install poetry==1.8.5
     poetry config virtualenvs.in-project true
     poetry install
 fi
